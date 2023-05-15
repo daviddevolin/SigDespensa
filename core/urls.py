2c8e9dca3bfeb3da46d4509ab1d338c310ffcb5e
david santos <56547716+daviddevolin@users.noreply.github.com>
2023-05-15 16:27:01 -0300

    feat: search user #9
    implementing user search by name

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, saveUser, listUsers, updateUser, update, deleteUser
+from .views import home, saveUser, listUsers, updateUser, update, deleteUser, searchUsers
 urlpatterns = [
     path('', home),
     path('salvar/', saveUser, name="salvar"),
     path('users/', listUsers, name="users"),
     path('editar/<int:id>', updateUser, name="editar"),
     path('update/<int:id>', update, name="update"),
-    path('delete/<int:id>', deleteUser, name="delete")
+    path('delete/<int:id>', deleteUser, name="delete"),
+    path('search/', searchUsers, name="search")
 ]

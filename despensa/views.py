9cb2c5d9a04a768ecc72552a90fedfc721f71d39
david santos <56547716+daviddevolin@users.noreply.github.com>
2023-06-13 16:25:08 -0300

    Update views.py

--- despensa/views.py
+++ despensa/views.py
@@ -2,15 +2,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Despensa
 from .forms import DespensaForm
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["GET", "POST"])
 def despensa_list(request):
     despensas = Despensa.objects.all()
     return render(request, 'despensa/list.html', {'despensas': despensas})
 
+@require_http_methods(["GET", "POST"])
 def despensa_detail(request, pk):
     despensa = get_object_or_404(Despensa, pk=pk)
     return render(request, 'despensa/detail.html', {'despensa': despensa})
 
+@require_http_methods(["GET", "POST"])
 def despensa_create(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
@@ -21,6 +25,7 @@ def despensa_create(request):
         return redirect('despensas:despensa_list')
     return render(request, 'despensa/form.html', {"form": DespensaForm()})
 
+@require_http_methods(["GET", "POST"])
 def despensa_update(request, pk):
     despensa = get_object_or_404(Despensa, pk=pk)
     if request.method == 'POST':
@@ -36,6 +41,7 @@ def despensa_update(request, pk):
         return redirect('despensas:despensa_list')
     return render(request, 'despensa/update.html', {'form': DespensaForm(), 'despensa': despensa})
 
+@require_http_methods(["GET", "POST"])
 def despensa_delete(request, pk):
     despensa = get_object_or_404(Despensa, pk=pk)
     if request.method == 'POST':

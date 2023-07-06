dbdc891f000153a0a4c49e65421310b5c0d471d9
Italo Guilherme <itinsmemes@gmail.com>
2023-07-06 09:51:35 -0300

    Despensa bug fixed #47

--- despensa/forms.py
+++ despensa/forms.py
@@ -5,3 +5,27 @@ class DespensaForm(ModelForm):
     class Meta:
         model = Despensa
         fields = '__all__'
+
+from typing import Any, Dict
+from django.forms import ModelForm, ValidationError
+from .models import Despensa
+
+class DespensaForm(ModelForm):
+    class Meta:
+        model = Despensa
+        fields = [ 'nome', 'quantTotal', 'capacidade',]
+
+    def clean_nome(self):
+        nome = self.cleaned_data.get('nome')
+        print('trying to add:', nome)
+
+        if (self.check_despensa_exists(nome)):
+            raise ValidationError(f"Nome de Despensa {nome} já existe", 'invalid')
+        return nome
+    
+    def check_user_exists(self, nome):
+        try:
+            Despensa.objects.get(nome=nome)
+            return True
+        except Despensa.DoesNotExist:
+            return False
--- despensa/views.py
+++ despensa/views.py
@@ -18,8 +18,8 @@ def despensa_list(request):
 
 @login_required
 @require_safe
-def despensa_detail(request, pk):
-    despensa = get_object_or_404(Despensa, pk=pk)
+def despensa_detail(request, id):
+    despensa = get_object_or_404(Despensa, id=id)
 
     rendered_page = render(request, 'despensa/detail.html', {'despensa': despensa})
 
@@ -38,6 +38,29 @@ def despensa_create(request):
     
     Despensa.objects.create(nome=nome, quantTotal=quantTotal, capacidade=capacidade)
     return redirect('despensas:despensa_list')
+
+#@require_POST
+#def despensa_create(request):
+
+    despensa_form = DespensaForm(request.POST)
+
+    if despensa_form.is_valid():
+        cleaned_data_despensa = despensa_form.cleaned_data
+
+        nome = cleaned_data_despensa.get('nome')
+        quantTotal = cleaned_data_despensa.get('quantTotal')
+        capacidade = cleaned_data_despensa.get('capacidade')
+
+        Despensa.objects.despensa_create(nome=nome,
+                                quantTotal=quantTotal,
+                                capacidade=capacidade,)
+
+        rendered_page = render(request, 'users/index.html', {"usuario_form": DespensaForm()})
+        return HttpResponse(rendered_page)
+    else:
+        print(despensa_form.errors)
+        rendered_page = render(request, 'users/index.html', {"usuario_form": DespensaForm(), "errors": despensa_form.errors})
+        return HttpResponse(rendered_page)
 
 @login_required
 @require_safe
